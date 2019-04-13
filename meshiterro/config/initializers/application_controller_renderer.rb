7b3585e815ca6a1fbb4bac2c61f0587e683ca944
原田浩輝 <haradahiroki@haradahirokinoMacBook-Pro.local>
2019-04-13 21:33:14 +0900

    [Start]5-5

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6854d9c5bfd7860943ef176c9c828ded3cee19dc677238787a2f45d54866f6c56efc7ece581caa2f645200cdfcbcc7950e314af50f7572b9771c2792077bb98f'
--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'post_images#index'
 
-resources :post_images, only: [ :new, :create, :index, :show] do
+resources :post_images, only: [ :new, :create, :index, :show, :destroy] do
 resources :favorites, only: [:create, :destroy]
 resource :post_comments, only: [:create, :destroy]
 end
+
+resources :users, only: [:show]
+
 end
 
 
